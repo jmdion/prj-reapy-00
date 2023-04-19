a7a14e0b012acf6c57db11b7c66d8a290f76d5b5
jmdion <jmdion@users.noreply.github.com>
2023-04-18 21:28:45 -0400

    Do not create new track. Add note to existing 'Keys' track

--- main.py
+++ main.py
@@ -7,16 +7,15 @@ def main():
 
     base_project_path = "R:/Production/Projets REAPER/base-project-00/base-project-00.rpp"
     prj = open_project(base_project_path)
-    prj.save(force_save_as=True)
+    # prj.save(force_save_as=True)
 
     prj.bpm = 60
 
-    track = prj.add_track(name="my track 2")
-    fx = track.add_fx("Kontakt (Native Instruments) (64 out)")
+    keys_track = prj.tracks['Keys']
 
     # fx.preset = r"R:\Production\Projets REAPER\prj-for-reapy-01\_me-simple.sine.nmsv"
 
-    item = track.add_midi_item(start=1.0, end=3.0)
+    item = keys_track.add_midi_item(start=1.0, end=3.0)
 
     take = item.takes[0]
 
